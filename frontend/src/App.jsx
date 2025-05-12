1319b945c2897bb41337ec12f74639ac0c34ee09
Brian Shimkus <brianshimkus1@gmail.com>
2025-05-12 14:22:37 -0500

    Get users from backend and display on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,37 @@
+import { useMutation, gql, useQuery } from '@apollo/client'
+
+const GET_USERS = gql`
+	query GetUsers {
+		getUsers {
+			id
+			age
+			name
+			isMarried
+		}
+	}
+`
+
 export default function App() {
-	return <div>App</div>
+	const { data, error, loading } = useQuery(GET_USERS)
+
+	if (loading) return <p>Loading...</p>
+
+	if (error) return <p>Error: {error.message}</p>
+
+	console.log(data.getUsers)
+
+	return (
+		<div>
+			<h1>Users</h1>
+			<div>
+				{data.getUsers.map((user) => (
+					<div key={user.id}>
+						<p>Name: {user.name}</p>
+						<p>Age: {user.age}</p>
+						<p>Married: {user.isMarried ? 'Yes' : 'No'}</p>
+					</div>
+				))}
+			</div>
+		</div>
+	)
 }
